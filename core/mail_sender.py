545d01d7446f072294cb77fddf826c99aa692cb8
ernestodtv <ernestodtv@gmail.com>
2020-07-05 13:01:39 +0200

    Add sendgrid config

--- core/mail_sender.py
+++ core/mail_sender.py
@@ -1,15 +1,32 @@
-from django.core.mail import EmailMessage
+import os
+import sendgrid
+
 
 def send_mail(applicant, collaborator):
     applicant_name = applicant.full_name
     email = collaborator.user.email
 
-    message = EmailMessage(
-                "Chronus: nueva colaboración",
-                f"{applicant_name} acaba de aceptar tu ofrecimiento de colaboración.",
-                "ernestodtv@gmail.com",
-                [email]
-            )
+    sg = sendgrid.SendGridClient(os.getenv('SENDGRID_API_KEY'))
+    message = sendgrid.Mail()
+    message.add_to(email)
+    message.set_from(os.getenv('SENDGRID_USERNAME'))
+    message.set_subject('Chronus: nueva colaboración')
+    message.set_html(f'{applicant_name} acaba de aceptar tu ofrecimiento de colaboración.')
+    sg.send(message)
 
-    message.send()
+
+# from django.core.mail import EmailMessage
+
+# def send_mail(applicant, collaborator):
+#     applicant_name = applicant.full_name
+#     email = collaborator.user.email
+
+#     message = EmailMessage(
+#                 "Chronus: nueva colaboración",
+#                 f"{applicant_name} acaba de aceptar tu ofrecimiento de colaboración.",
+#                 "ernestodtv@gmail.com",
+#                 [email]
+#             )
+
+#     message.send()
    
